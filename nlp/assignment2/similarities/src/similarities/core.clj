b7bd8379e9a935b78dab5132788a9652bc36b7dc
Fabian Bornhofen <fbornhofen@googlemail.com>
2012-06-15 16:20:11 +0200

    extract vocabulary

--- nlp/assignment2/similarities/src/similarities/core.clj
+++ nlp/assignment2/similarities/src/similarities/core.clj
@@ -1,11 +1,26 @@
 (ns similarities.core)
 
+
 (use '[clojure.string :only [split]])
+(use 'clojure.set)
 
 (import 'java.io.BufferedReader)
 (import 'java.io.FileReader)
 
-(defrecord WordOccurrence [word position pos-tag context])
+(defrecord VocabularyWord [word positions pos-tags context-words])
+
+
+(defn vw-update [vw word position pos-tag context-words]
+  (if (nil? vw)
+    (->VocabularyWord word
+                      [position]
+                      (hash-set pos-tag)
+                      (set context-words))
+    (->VocabularyWord word
+                      (conj (:positions vw) position)
+                      (conj (:pos-tags vw) pos-tag)
+                      (union (:context-words vw) context-words))))
+
 
 (defn read-lines [file-name]
   (with-open [rdr (BufferedReader. (FileReader. file-name))]
@@ -15,5 +30,21 @@
   (map (fn [line] (split line (re-pattern "\\s")))
        array-of-lines))
 
+(defn all-words [array-of-lines]
+  (reduce concat (tokenized-lines array-of-lines)))
+
 (defn number-of-words-in-file [file-name]
   (reduce + (map count (tokenized-lines (read-lines file-name)))))
+
+
+(defn extract-vocabulary [words]
+  (reduce (fn [s e]
+            (let [word-and-tag (split e (re-pattern "/"))
+                  word (first  word-and-tag)
+                  tag  (second word-and-tag)]
+                                        ; (s word) will yield nil if not present
+                                        ; -> no need to check
+              (assoc s word (vw-update (s word) word 0 tag []))))
+          {}
+          words))
+
