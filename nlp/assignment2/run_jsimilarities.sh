319844ef3edc8f937f79da472d91fdaeeffb2195
Fabian Bornhofen <fbornhofen@googlemail.com>
2012-06-19 09:54:31 +0200

    smaller heap size will do for now. can optimize further (see comments).

--- nlp/assignment2/run_jsimilarities.sh
+++ nlp/assignment2/run_jsimilarities.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd JSimilarities/bin
-time java -Xmx8096m de.bfabian.similarities.Main ../../data/5000lines.txt context.txt word-pairs.txt 4 1 500
+time java -Xmx4096m de.bfabian.similarities.Main ../../data/5000lines.txt context.txt word-pairs.txt 4 1 500
